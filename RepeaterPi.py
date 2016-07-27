c6f8468206bc73e55c60dd289ab731719407658d
Ellsworth <EllsworthTheBox@gmail.com>
2016-07-27 15:47:47 -0500

    voltage converter done, still need sensor hookups. Will come after completion of first sensor board.

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -1,7 +1,11 @@
 print("RepeaterPi v.1 by KG5KEY")
 
-raw_sensor_temp1 = 2.2
-raw_sensor_volt1 = 1.5
+raw_sensor_CH1_voltage = 2.3
+raw_sensor_CH1_max_voltage = 16
 
-def calcSensorReadout():
+
+def calc_voltage(sensor_readout, max_voltage):
+    return (raw_sensor_CH1_voltage * raw_sensor_CH1_max_voltage) / 3.3
+
+print(calc_voltage(raw_sensor_CH1_voltage, raw_sensor_CH1_max_voltage))
 

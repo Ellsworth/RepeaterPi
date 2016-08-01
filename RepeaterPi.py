602308d4fb422fb045307e0a81ca31e1c3bb5d38
Ellsworth <EllsworthTheBox@gmail.com>
2016-08-01 00:15:04 -0500

    now supports io.adafruit.com!

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -1,26 +1,31 @@
 import configparser
-#import spidev
+# mport spidev
 import time
-import os
+# import os
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-print("RepeaterPi v.1 by KG5KEY on " + config['Basic']['RepeaterName'])
+print("RepeaterPi v.1 by KG5KEY on " + config['Basic']['repeater_name'])
 
 # nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
 raw_sensor_CH0_voltage = 2.502
-raw_sensor_CH1_voltage = 3.3
+raw_sensor_CH1_voltage = 2.9
+temp = 95
 
 
 def calc_voltage(sensor_readout):
     return round((sensor_readout * 16) / 2.9, 2)
 
+def calc_temp(data):
+    return (data * (9/5)) + 32  # converts C to F cause MURICA
 
-print("--------------------------------------")
-print("Telemetry for " + str(time.asctime(time.localtime(time.time())))) # comrade
-print("Primary: " + str(calc_voltage(raw_sensor_CH0_voltage)) +
-      "v Amplifier: " + str(calc_voltage(raw_sensor_CH1_voltage)) + "v")
-print("--------------------------------------")
+def gen_Telemetry():
+    return ("-------------------------------------- \nTelemetry for " +
+          str(time.asctime(time.localtime(time.time()))) +
+          "\nPrimary: " + str(calc_voltage(raw_sensor_CH0_voltage)) +
+          "v Amplifier: " + str(calc_voltage(raw_sensor_CH1_voltage)) +
+          "v" + "\nTemperature: " + str(temp)) + " Degrees Fahrenheit" + "\n--------------------------------------"
 
 
+print(gen_Telemetry())
---
+++ RepeaterPi_Service.py
@@ -0,0 +1,46 @@
+import RepeaterPi
+import configparser
+from time import sleep
+from Adafruit_IO import Client
+
+print("\nStarting RepeaterPi service...")
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+aio = Client(config['AdafruitIO']['AIO_Key'])
+
+temp = 73
+main_power = 13.8
+amplifier_power = 13.8
+
+# purely testing
+new_temp = 73
+new_main_power = 13.8
+new_amplifier_power = 0
+
+
+def updateAdafruitIO():
+    try:
+        aio.send(config['Basic']['repeater_location'] + '-temp', temp)
+        aio.send(config['Basic']['repeater_location'] + '-main-power', main_power)
+        aio.send(config['Basic']['repeater_location'] + '-amplifier-power', amplifier_power)
+    except:
+        print("no internets :C")
+
+
+# while True:
+#    print(RepeaterPi.gen_Telemetry())
+#    updateAdafruitIO()
+#    sleep(300)
+
+updateAdafruitIO()
+
+while True:
+    sleep(5)
+    if new_temp != temp or new_main_power != main_power or new_amplifier_power != amplifier_power:
+        print("Data out of date, running updateAdafruitIO() to fix that...")
+        temp = new_temp
+        main_power = new_main_power
+        amplifier_power = new_amplifier_power
+        updateAdafruitIO()

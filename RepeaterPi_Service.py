d26802bba6eb253e2f16a62f4fac5cb0544037d5
Erich Ellsworth <ellsworthes@aol.com>
2016-08-07 05:43:38 +0000

    fixed ( error

--- RepeaterPi_Service.py
+++ RepeaterPi_Service.py
@@ -18,7 +18,7 @@ amplifier_power = 13.8
 
 def updateAdafruitIO():
     try:
-        aio.send(config['Basic']['repeater_location'] + '-temp', int(round(RepeaterPi.calc_temp(7), 0))
+        aio.send(config['Basic']['repeater_location'] + '-temp', int(round(RepeaterPi.calc_temp(7), 0)))
         aio.send(config['Basic']['repeater_location'] + '-main-power', main_power)
         aio.send(config['Basic']['repeater_location'] + '-amplifier-power', amplifier_power)
         print("Updating AdafruitIO...")

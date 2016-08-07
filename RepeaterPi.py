85c42ebb2d197144d7879eebf195ffe7b678e10b
Erich Ellsworth <ellsworththebox@gmail.com>
2016-08-07 05:40:05 +0000

    not sure what im doing

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -19,10 +19,10 @@ def get_voltage(channel):
 
 # nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
 def scale_voltage(sensor_readout):
-    return float((sensor_readout * 16) / 2.9, 2)
+    return (sensor_readout * 16) / 2.9
 
 def calc_temp(channel):
-    return float(((((get_voltage(7) * 1000) - 500) / 10) * 9 / 5) + 32, 0)
+    return ((((get_voltage(7) * 1000) - 500) / 10) * 9 / 5) + 32
  
     #return (get_voltage(channel) * (9/5)) + 32  # converts C to F cause MURICA
 
@@ -32,3 +32,5 @@ def gen_Telemetry():
           "\nPrimary: " + str("0") +
           "v Amplifier: " + str(0) +
           "v" + "\nTemperature: " + str(calc_temp(7)) + " Degrees Fahrenheit" + "\n--------------------------------------")
+
+
--- RepeaterPi_Service.py
+++ RepeaterPi_Service.py
@@ -18,21 +18,17 @@ amplifier_power = 13.8
 
 def updateAdafruitIO():
     try:
-        aio.send(config['Basic']['repeater_location'] + '-temp', RepeaterPi.calc_temp(7))
+        aio.send(config['Basic']['repeater_location'] + '-temp', int(round(RepeaterPi.calc_temp(7), 0))
         aio.send(config['Basic']['repeater_location'] + '-main-power', main_power)
         aio.send(config['Basic']['repeater_location'] + '-amplifier-power', amplifier_power)
+        print("Updating AdafruitIO...")
+        print(RepeaterPi.gen_Telemetry())
     except:
         print("no internets :C")
 
-# while True:
-#    print(RepeaterPi.gen_Telemetry())
-#    updateAdafruitIO()
-#    sleep(300)
 
 updateAdafruitIO()
 
 while True:
-    sleep(300)
-    print('Updating AdafruitIO...')
+    sleep(5)
     updateAdafruitIO()
-    

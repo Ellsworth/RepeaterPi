eebd17b4de4ff931b9fbdf39d7536668777daeb7
Erich Ellsworth <ellsworthes@aol.com>
2016-08-07 04:31:38 +0000

    ...

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -24,7 +24,7 @@ def scale_voltage(sensor_readout):
 
 def calc_temp(channel):
     #return ((mcp.read_adc(channel) / 1023) * 150) * (9/5) + 32
-    return round(((get_voltage(7) * 1000) - 500 / 10))
+    return ((get_voltage(7) * 1000) - 500) / 10
  
     #return (get_voltage(channel) * (9/5)) + 32  # converts C to F cause MURICA
 
@@ -35,32 +35,3 @@ def gen_Telemetry():
           "v Amplifier: " + str(0) +
           "v" + "\nTemperature: " + str(calc_temp(7))) + " Degrees Fahrenheit" + "\n--------------------------------------"
 
-
-
-
-## Testing  Raspberry Spy code
-def ConvertVolts(data):
-  volts = (data * 3.3) / float(1023)
-  return volts
- 
-# Function to calculate temperature from
-# TMP36 data, rounded to specified
-# number of decimal places.
-def ConvertTemp(data):
- 
-  # ADC Value
-  # (approx)  Temp  Volts
-  #    0      -50    0.00
-  #   78      -25    0.25
-  #  155        0    0.50
-  #  233       25    0.75
-  #  310       50    1.00
-  #  465      100    1.50
-  #  775      200    2.50
-  # 1023      280    3.30
- 
-  temp = ((data * 330)/float(1023))-50
-  return temp
-
-print(ConvertTemp(ConvertVolts(mcp.read_adc(7))))
-print(calc_temp(7))

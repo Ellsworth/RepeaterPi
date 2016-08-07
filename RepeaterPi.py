579bd96994c4a0bc7f3073bdc16984df05b61d77
Erich Ellsworth <ellsworthes@aol.com>
2016-08-07 05:01:04 +0000

    Added F conversion cause MURICA

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -13,18 +13,16 @@ config.read('config.ini')
 
 print("RepeaterPi v.1 by KG5KEY on " + config['Basic']['repeater_name'])
 
-# nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
-
 
 def get_voltage(channel):
-    return ((mcp.read_adc(channel) * 3.3) / 1023) / (.635 / .75)
+    return ((mcp.read_adc(channel) * 3.3) / 1023) / (.635 / .75)   # WTF?!?
 
+# nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
 def scale_voltage(sensor_readout):
     return round((sensor_readout * 16) / 2.9, 2)
 
 def calc_temp(channel):
-    #return ((mcp.read_adc(channel) / 1023) * 150) * (9/5) + 32
-    return ((get_voltage(7) * 1000) - 500) / 10
+    return ((((get_voltage(7) * 1000) - 500) / 10) * 9 / 5) + 32
  
     #return (get_voltage(channel) * (9/5)) + 32  # converts C to F cause MURICA
 

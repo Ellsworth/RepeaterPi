1117277379fb193dfa6502ef12b004eb1aa94203
Erich Ellsworth <ellsworththebox@gmail.com>
2016-08-21 01:49:57 +0000

    cleaned up some code

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -15,7 +15,7 @@ print("RepeaterPi v.1 by KG5KEY on " + config['Basic']['repeater_name'])
 
 
 def get_voltage(channel):
-  return (mcp.read_adc(channel) * config['Basic']['voltage33']) / 1023
+  return (mcp.read_adc(channel) * float(config['Basic']['voltage33'])) / 1023
 
 # nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
 def scale_voltage(channel):
@@ -31,3 +31,5 @@ def gen_Telemetry():
           "v Amplifier: " + str(scale_voltage(1)) +
           "v" + "\nTemperature: " + str(calc_temp(7)) + " Degrees Fahrenheit" + "\n--------------------------------------")
 
+print(gen_Telemetry())
+print(get_voltage(7))

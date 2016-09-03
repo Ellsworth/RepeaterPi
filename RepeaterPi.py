dddfadd57e7978ee7d4995cd32a7dc40d2577c47
Erich Ellsworth <ellsworthes@aol.com>
2016-09-03 21:03:53 +0000

    various changes to the way config values are stored, also added calibration to power sensors

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -9,10 +9,13 @@ mcp = Adafruit_MCP3008.MCP3008(spi=Adafruit_GPIO.SPI.SpiDev(0, 0))
 
 # Config configuration
 config = configparser.ConfigParser()
-config.read('/root/RepeaterPi//config.ini')
+config.read('/root/RepeaterPi/config.ini')
 aio = Client(config['AdafruitIO']['AIO_Key'])
+repeater_location = config['Basic']['repeater_name']
+main_cal = config['Basic']['main_cal']
+amplifier_cal = config['Basic']['amplifier_cal']
 
-print("RepeaterPi v.1 by KG5KEY on " + config['Basic']['repeater_name'])
+print("RepeaterPi v.1 by KG5KEY on " + repeater_location)
 
 
 # defining core funtions
@@ -21,14 +24,14 @@ def get_voltage(channel):
 
 # nominal voltage should be 13.8v aka 2.502v as read directly by the ADC...
 def scale_voltage(channel):
-    voltage = round(((get_voltage(channel) * 16) / 2.615), 2)
+    voltage = round(((get_voltage(channel) * 16) / 2.615), 2) # not sure why this works... but it does so im going to let it be
     if (voltage < 1):
-        voltage = 0
+        voltage = 0.0
     else:
         return voltage
 
 def calc_temp(channel):
-    return round(((((get_voltage(7) * 1000) - 500) / 10)) * 9 / 5 + 32, 1)
+    return round(((((get_voltage(7) * 1000) - 500) / 10)) * 9 / 5 + 32, 1) # I actually understand this, unlike scale_voltage()
 
  
 def gen_Telemetry():
@@ -39,9 +42,9 @@ def gen_Telemetry():
           "v" + "\nTemperature: " + str(calc_temp(7)) + " Degrees Fahrenheit" + "\n--------------------------------------")
 
 def updateAdafruitIO():
-    aio.send(config['Basic']['repeater_location'] + '-temp', calc_temp(7))
-    aio.send(config['Basic']['repeater_location'] + '-main-power', scale_voltage(0))
-    aio.send(config['Basic']['repeater_location'] + '-amplifier-power', scale_voltage(1))
+    aio.send(repeater_location + '-temp', calc_temp(7))
+    aio.send(repeater_location + '-main-power', (scale_voltage(0) * main_cal))
+    aio.send(repeater_location + '-amplifier-power', (scale_voltage(1) * amplifier_cal))
     print("[" + str(time.asctime(time.localtime(time.time()))) + "] Updating AdafruitIO...")
 
 

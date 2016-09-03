b078f85e07ee86fe203e402d920f3eb4e399e1e7
Erich Ellsworth <ellsworthes@aol.com>
2016-09-03 21:40:20 +0000

    temp fix? pt2

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -34,9 +34,9 @@ def calc_temp(channel):
     return round(((((get_voltage(7) * 1000) - 500) / 10)) * 9 / 5 + 32, 1) # I actually understand this, unlike scale_voltage()
  
 
-temp = calc_temp(7)
-main_power = float(scale_voltage(0)) * float(main_cal)
-amplifier_power = float(scale_voltage(1)) * float(amplifier_cal)
+temp = round(calc_temp, 2)
+main_power = round(float(scale_voltage(0)) * float(main_cal), 2)
+amplifier_power = round(float(scale_voltage(1)) * float(amplifier_cal), 2)
     
 def gen_Telemetry():
     return ("-------------------------------------- \nTelemetry for " +
@@ -57,7 +57,6 @@ print("\nStarting RepeaterPi service...")
 while True:
     updateAdafruitIO(temp, main_power, amplifier_power)
     time.sleep(300)
-    temp = calc_temp(7)
-    main_power = float(scale_voltage(0)) * float(main_cal)
-    amplifier_power = float(scale_voltage(1)) * float(amplifier_cal)
-
+    temp = round(calc_temp, 2)
+    main_power = round(float(scale_voltage(0)) * float(main_cal), 2)
+    amplifier_power = round(float(scale_voltage(1)) * float(amplifier_cal), 2)

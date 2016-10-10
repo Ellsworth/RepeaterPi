d5f2081678e06686ff04fcac0c45d13e89570972
Erich KG5KEY <kg5key@yahoo.com>
2016-10-10 10:07:31 -0500

    kalman filter actually works now.

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -15,8 +15,6 @@ repeater_location = config['Basic']['repeater_location']
 main_cal = config['Basic']['main_cal']
 amplifier_cal = config['Basic']['amplifier_cal']
 
-# temp: 0, old_temp: 1, main_power: 2, amp_power: 3
-
 # average: 0, 1:1, 2:2, 3:3, 4:4, 5:5
 tempHistory = [0, 0, 0, 0, 0, 0, 0]
 voltage = [0, 0] # primary 0, amp, 1
@@ -29,6 +27,7 @@ print("RepeaterPi 1.2v by KG5KEY on " + config['Basic']['repeater_name'])
 def get_voltage(channel):
     return (mcp.read_adc(channel) * 3.3) / float(1023)
 
+
 def scale_voltage(channel):
     voltage = ((get_voltage(channel) * 16) / float(3.3))
     if voltage < 1:
@@ -36,21 +35,26 @@ def scale_voltage(channel):
     else:
         return float(voltage)
 
+
 def calc_temp(channel):
-    return float(((((get_voltage(7) * 1000) - 500) / 10)) * 9 / 5 + 32)
+    return float(((((get_voltage(channel) * 1000) - 500) / 10) * 9 / 5 + 32)
+
 
 def gen_Telemetry():
     return ("-------------------------------------- \nTelemetry for " +
           str(time.asctime(time.localtime(time.time()))) +
           "\nPrimary: " + str(voltage[0]) +
           "v Amplifier: " + str(voltage[1]) +
-          "v" + "\nTemperature: " + str(tempHistory[0]) + " Degrees Fahrenheit\n")
+          "v" + "\nTemperature: " + str(tempHistory[0]) + " Degrees Fahrenheit\nTemperature Average: " +
+            tempHistory +"\n")
+
 
 def updateAdafruitIO():
-    aio.send(repeater_location + '-temp', tempHistory[1])
+    aio.send(repeater_location + '-temp', tempHistory[0])
     aio.send(repeater_location + '-main-power', voltage[0])
     aio.send(repeater_location + '-amplifier-power', voltage[1])
     print(gen_Telemetry())
+
 
 def isValid(current, previous):
     return abs(((current - previous) / previous) * 100) < 8
@@ -60,6 +64,7 @@ def updateSensors():
     voltage[0] = (round(float(scale_voltage(0)) * float(main_cal), 2))
     voltage[1] = (round(float(scale_voltage(1)) * float(amplifier_cal), 2))
     kalman(tempHistory)
+
 
 def kalman(var):
     # 0 and 1 is the new var, and 0 is the rounded amount from vars 2-6
@@ -74,9 +79,9 @@ def kalman(var):
 
 print("\nStarting RepeaterPi service...")
 print("Calibrating temperature sensor...")
-while x < 5:
+while x < 4:
     updateSensors()
-    x = x + 1
+    x += 1
     time.sleep(1.5)
 print("Finished calibrating temperature sensor.")
 

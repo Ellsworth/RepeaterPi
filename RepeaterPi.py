35babe85f07124db430191316c19b8134a34d237
Erich Ellsworth <ellsworthes@aol.com>
2016-10-10 01:04:44 +0000

    added rounding

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -67,7 +67,7 @@ def kalman(var):
     var[4] = var[3]
     var[3] = var[2]
     var[2] = var[1]
-    var[0] = (var[2] + var[3] + var[4] + var[5] + var[6]) / 5
+    var[0] = round((var[2] + var[3] + var[4] + var[5] + var[6]) / 5, 2)
     return var[0] # return rounded values
 
 

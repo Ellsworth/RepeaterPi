58740c4f08a6b7d38049d3ed3da490a11724ed46
Ellsworth <kg5key@yahoo.com>
2017-04-22 14:56:12 -0500

    now factoring voltage of the arduino

--- required_files/RepeaterPi/RepeaterPi.ino
+++ required_files/RepeaterPi/RepeaterPi.ino
@@ -3,11 +3,25 @@ int mainPin = 6;
 int ampPin = 7;
 String output;
 
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1125300L / result; // Back-calculate AVcc in mV
+  result = result / 10;
+  return result;
+}
+
 void setup()
 {
   Serial.begin(9600);  //Start the serial connection to the Pi
   while (!Serial) {
-  ; // wait for serial port to connect. Needed for native USB port only
+  ; // wait for serial port to connect.
 }
 
 }
@@ -19,6 +33,8 @@ void loop()
   Serial.print(",");
   Serial.print(analogRead(mainPin));
   Serial.print(",");
-  Serial.println(analogRead(ampPin));
+  Serial.print(analogRead(ampPin));
+  Serial.print(",");
+  Serial.println( readVcc(), DEC );
   delay(6000);
 }

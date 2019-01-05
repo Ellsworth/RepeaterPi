db4441b222900dba504cb4bdd054a05556619301
Erich Ellsworth <erich.ellsworth@g.austincc.edu>
2019-01-04 20:36:18 -0600

    Fixed!

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -131,7 +131,7 @@ def updateDashboard():
                 "temp_pi": float(getPiTemp()),
                 "v_main": voltage[0],
                 "v_amp": voltage[1],
-                "arduino": str(arduinoData[5]),
+                "arduino": str(float(arduinoData[5]) * 0.001),
                 "pwr_fwd": pwr_fwd,
                 "pwr_rev": pwr_rev,
             }

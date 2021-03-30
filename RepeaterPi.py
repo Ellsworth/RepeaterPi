12c4ca1578b2479e209b8029b310d8682a264a0c
Erich Ellsworth <kg5key@kg5key.com>
2021-03-30 18:13:05 -0500

    Partial rewrite for v3

--- RepeaterPi.py
+++ RepeaterPi.py
@@ -1,19 +1,19 @@
-from influxdb import InfluxDBClient
-from subprocess import check_output
-import configparser
-import time
 import serial
-import sys
 
-__version__ = "2.6"
-__author__ = "Erich Ellsworth"
-__contact__ = "erich.ellsworth@g.austincc.edu"
+import time
+from math import sqrt
+
+from subprocess import check_output
+from influxdb import InfluxDBClient
+
+import configparser
+
+__version__ = "3"
+__author__ = "Erich Ellsworth, KG5KEY"
+__contact__ = "kg5key@kg5key.com"
 
 # Need to tell where to find the cfg if running from systemd
-config_file = '/root/RepeaterPi/config.ini'
-
-if len(sys.argv) > 1 and sys.argv[1] == "--test":
-        config_file = './config_example.ini'
+config_file = 'config.ini'
 
 config = configparser.ConfigParser()
 config.read(config_file)
@@ -22,14 +22,13 @@ config.read(config_file)
 repeater_location = config['Basic']['repeater_location']
 repeater_name = config['Basic']['repeater_name']
 serial_port = config['Basic']['serial_port']
-poll_time_sec = int(config['Basic']['poll_time_sec'])
 
-main_cal = config['calibration']['main_cal']
-amplifier_cal = config['calibration']['amplifier_cal']
-fwd_pwr_cal = config['calibration']['pwr_fwd']
-rev_pwr_cal = config['calibration']['pwr_rev']
-
-
+serial_calibration = [(config['calibration']['temp_cal']),
+    (config['calibration']['pres_cal']),
+    (config['calibration']['main_cal']),
+    (config['calibration']['amplifier_cal']),
+    (config['calibration']['pwr_fwd']),
+    (config['calibration']['pwr_rev'])]
 
 
 # InfluxDB Login
@@ -47,64 +46,40 @@ serialPort = serial.Serial()
 
 # average is 0, most recent 1, least recent 0
 serialdata = ""
-arduinoData = [0, 0, 0, 0, 0, 0] # 0 temp, 1 main, 2 amp, 3 fwd, 4 rev, 5 5v ref
-tempHistory = [0, 0, 0, 0, 0, 0]
+# temp_f, pressure, main.voltage, amp.voltage, fwd.voltage, rev.voltage
+arduinoData = [0, 0, 0, 0, 0, 0] # 0 temp_f, 1 pressure_mb, 2 main, 3 amp, 4 fwd, 5 rev,
 voltage = [0, 0, 0, 0]
 x = 0
-tx = True
-startup = True
+
+converted_data = [0,0,0,0,0,0]
 
 # Intro message
 print("RepeaterPi %sv by KG5KEY on %s\n" % (__version__, repeater_name))
-print("Copyright (C) 2017 Erich Ellsworth\n" +
-    "Contact: " + __contact__)
 
-# gets the data from the ADC and converts it to raw voltage
-def getVoltage(channel):
-    return (float(arduinoData[channel]) * (float(arduinoData[5]) * .001) / float(1023))
-    #return float((float(arduinoData[channel]) * 5.0 / float(1023)))
-
-def scaleVoltage(channel):
-    rv = (getVoltage(channel) * (61/11))
+def scaleVoltage(voltage):
+    rv = (voltage * (61/11))
     if rv < 1:
         rv = 0
     return rv
 
-def scaleWattage(channel):
-    return ((75 * getVoltage(channel)) / 3.3)
-
-# calculates temp
-def calcTemp(channel):
-    temp = round(float(((((getVoltage(channel) * 1000) - 500) / 10) * 9 / 5 + 32)), 2)
-    if abs(temp - tempHistory[0]) > 4:
-        temp = tempHistory[0]
-    return float(temp)
+def scaleWattage(voltage):
+    return ((75 * voltage) / 3.3)
 
 
 def getPiTemp():
-    cputemp = check_output(['/opt/vc/bin/vcgencmd', 'measure_temp'])
-    cputemp = str(cputemp[5:9])
-    for char in "b'":
-        cputemp = cputemp.replace(char,'')
+    #cputemp = check_output(['/opt/vc/bin/vcgencmd', 'measure_temp'])
+    #cputemp = str(cputemp[5:9])
+    #for char in "b'":
+    #    cputemp = cputemp.replace(char,'')
+    cputemp = 0
     return cputemp
 
-
-def genTelemetry():
-    print("--------------------------------------")
-    print("Telemetry for " + str(time.asctime(time.localtime(time.time()))))
-    print("Temperature: " + str(tempHistory[0]) + " Pi Temp: " + str(getPiTemp()))
-    print("Primary: " + str(voltage[0]) + "v Amplifier: " + str(voltage[1]) + "v")
-    print("fwd " + str(pwr_fwd) +  " rev " + str(pwr_rev))
-    print("Arduino Data: " + str(arduinoData))
-    print("x = " + str(x) + " tx: " + str(tx))
-
-
-
-def calibrateTemp(channel):
-    temp = round(float(((((getVoltage(channel) * 1000) - 500) / 10) * 9 / 5 + 32)), 2)
-    return ([temp, temp, temp, temp, temp, temp])
-
-
+def calc_swr(fwd, rev):
+    if fwd <= 0 or rev <= 0 or (fwd - rev) <= 0:
+        return float(1.00)
+    else:
+        swr = (1 + sqrt(rev / fwd)) / (1 - sqrt(rev / fwd))
+        return round(float(swr), 2)
 
 def getSerialData():
     serialdata = str(serialPort.readline())
@@ -112,20 +87,23 @@ def getSerialData():
         serialdata = serialdata.replace(char,'')
     for char in "\\":
         serialdata = serialdata.replace(char,'')
-    return(serialdata.split(","))
+    return(serialdata.split(" "))
+
+def convertData(data):
+
+    converted_data[0] = data[0] # Temperature
+    converted_data[1] = data[1] # Pressure
+
+    converted_data[2] = round(scaleVoltage(data[2]), 2) # Primary Voltage
+    converted_data[3] = round(scaleVoltage(data[3]), 2) # Amp Voltage
+
+    converted_data[4] = round(scaleWattage(data[4]), 1) # Forward Power
+    converted_data[5] = round(scaleWattage(data[5]), 1) # Reverse Power
+
+    return converted_data
 
 
-def tempAverage(var):
-    tempHistory[5] = tempHistory[4]
-    tempHistory[4] = tempHistory[3]
-    tempHistory[3] = tempHistory[2]
-    tempHistory[2] = tempHistory[1]
-    tempHistory[1] = var
-    tempHistory[0] = round((tempHistory[1] + tempHistory[2] +
-        tempHistory[3] + tempHistory[4] + tempHistory[5]) / 5, 2)
-    return tempHistory
-
-def updateDashboard():
+def updateDashboard(info):
     json_body = [
         {
             "measurement": repeater_location,
@@ -133,46 +111,28 @@ def updateDashboard():
                 "use": "null",
             },
             "fields": {
-                "temp": calcTemp(0),
-                "temp_pi": float(getPiTemp()),
-                "v_main": voltage[0],
-                "v_amp": voltage[1],
-                "arduino": float('%.5s' % str(float(arduinoData[5]) * float(.001))),
-                "pwr_fwd": pwr_fwd,
-                "pwr_rev": pwr_rev,
+                "temperature": float(info[0]),
+                "pressure": float(info[1]),
+                "temperature_pi": float(getPiTemp()),
+                "voltage_main": float(info[2]),
+                "voltage_amp": float(info[3]),
+                "arduino": 0,
+                "pwr_fwd": info[4],
+                "pwr_rev": info[5],
+                "SWR": calc_swr(info[4], info[5]),
             }
         }
     ]
-    print("Sending data to " + hostname)
+    print("Sending data to " + hostname + "...", end="")
 
     client.write_points(json_body)
-    print("Done sending data.")
+    print(" Done sending data.")
 
+def printData(info):
+    print("Temperature: %s F, Pressure: %s mb" % (info[0], info[1]))
+    print("Main Supply: %s V, Amplifier Supply: %s V" % (info[2], info[3]))
+    print("Forward Power: %s W, Reverse Power: %s W, SWR: %s" % (info[4], info[5], calc_swr(info[4],info[5])))
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "--copyright":
-        print("\nThis program is free software: you can redistribute it and/or modify\n" +
-            "it under the terms of the GNU General Public License as published by\n" +
-            "the Free Software Foundation, either version 3 of the License, or\n" +
-            "(at your option) any later version.\n\n" +
-            "This program is distributed in the hope that it will be useful,\n" +
-            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
-            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
-            "GNU General Public License for more details.\n\n" +
-            "You should have received a copy of the GNU General Public License\n" +
-            "along with this program. If not, see <http://www.gnu.org/licenses/>.")
-    if sys.argv[1] == "--test":
-        print("\n--- Start Report  ---")
-        print("Python: " + sys.version)
-        print("PySerial: " + str(serial.__version__))
-        print("RepeaterPi: " + str(__version__))
-        print("argv: " + str(sys.argv))
-        print("--- End of Report ---")
-
-    sys.exit(0)
-
-print("To view the copyright information, run RepeaterPi.py with the " +
-    "--copyright argument.")
 
 
 if __name__ == '__main__':
@@ -184,42 +144,15 @@ if __name__ == '__main__':
     serialPort.port = serial_port
     serialPort.open()
 
-    x = 0
-    startup = True
-
-    print("Reading data from Arduino, this may take up to 15 seconds.")
-
-    arduinoData = getSerialData()
-    tempHistory = calibrateTemp(0)
-
-    voltage[0] = (round(float(scaleVoltage(1)) * float(main_cal), 2))
-    voltage[1] = (round(float(scaleVoltage(2)) * float(amplifier_cal), 2))
-    voltage[2] = voltage[0]
-    voltage[3] = voltage[1]
-    pwr_fwd = (scaleWattage(3) * float(fwd_pwr_cal))
-    pwr_rev = (scaleWattage(4) * float(rev_pwr_cal))
-    updateDashboard()
-
-    startup = False
-
     while True:
-        arduinoData = getSerialData()
-        tempAverage(calcTemp(0))
+        #"temperature,voltage_main,voltage_amp,forward_power,reverse_power")
+        serial_raw = getSerialData()
+        print('-----------------------------------------------------')
 
-        voltage[0] = (round(float(scaleVoltage(1)) * float(main_cal), 2))
-        voltage[1] = (round(float(scaleVoltage(2)) * float(amplifier_cal), 2))
-        pwr_fwd = (scaleWattage(3) * float(fwd_pwr_cal))
-        pwr_rev = (scaleWattage(4) * float(rev_pwr_cal))
+        data = [float(serial_raw[i]) + float(serial_calibration[i]) for i in range(len(serial_raw))]
 
-        genTelemetry()
-        if abs(voltage[0] - voltage[2]) > .3 or abs(voltage[1] - voltage[3]) > .3 or x > ((900 / poll_time_sec) - 1) or tx or pwr_fwd > 1:
+        converted_data = convertData(data)
 
-            updateDashboard()
-            voltage[2] = voltage[0]
-            voltage[3] = voltage[1]
-            x = 0
-        else:
-            x += 1
-
-        tx = pwr_fwd > 1
-        time.sleep(poll_time_sec)
+        updateDashboard(converted_data)
+        print(serial_raw)
+        printData(converted_data)
